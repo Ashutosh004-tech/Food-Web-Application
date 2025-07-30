1010371ebaf6425342958110e2b3b12e224a23a7
Ashutosh Panigrahi <ashutoshpanigrahi11@gmail.com>
2025-07-30 20:34:28 +0530

    Intial Commit

---
+++ init/initialData.js
@@ -0,0 +1,74 @@
+const biryaniMenu = [
+    {
+      item: "Hyderabadi Dum Biryani",
+      description: "Fragrant basmati rice layered with spiced chicken and slow-cooked to perfection.",
+      image: "https://images.unsplash.com/photo-1589302168068-964664d93dc0?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      price: 220,
+      owner:"68123b126e02a3ad3e0a73e6",
+    },
+    {
+      item: "Lucknowi Biryani",
+      description: "Mildly spiced biryani with tender meat, infused with saffron and kewra.",
+      image: "https://images.unsplash.com/photo-1708184528306-f75a0a5118ee?q=80&w=2010&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      price: 240,
+      owner:"68123b126e02a3ad3e0a73e6",
+    },
+    {
+      item: "Kolkata Biryani",
+      description: "Biryani with potato, subtle spices, and aromatic rice inspired by Nawabi flavors.",
+      image: "https://images.unsplash.com/photo-1610057099443-fde8c4d50f91?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      price: 200,
+      owner:"68123b126e02a3ad3e0a73e6",
+    },
+    {
+      item: "Chicken Tikka Biryani",
+      description: "Grilled chicken tikka chunks layered with flavorful biryani rice.",
+      image: "https://plus.unsplash.com/premium_photo-1668146932661-2c7e3608ec51?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YmlyaXlhbml8ZW58MHx8MHx8fDA%3D",
+      price: 260,
+      owner:"68123b126e02a3ad3e0a73e6",
+    },
+    {
+      item: "Mutton Biryani",
+      description: "Juicy mutton pieces cooked with long-grain rice and rich spices.",
+      image: "https://images.unsplash.com/photo-1610057099443-fde8c4d50f91?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      price: 280,
+      owner:"68123b126e02a3ad3e0a73e6",
+    },
+    {
+      item: "Paneer Biryani",
+      description: "A vegetarian delight with paneer cubes and aromatic masala rice.",
+      image: "https://images.unsplash.com/photo-1630409346824-4f0e7b080087?q=80&w=2146&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      price: 190,
+      owner:"68123b126e02a3ad3e0a73e6",
+    },
+    {
+      item: "Egg Biryani",
+      description: "Boiled eggs cooked in spicy gravy and layered with fragrant rice.",
+      image: "https://images.unsplash.com/photo-1642972420043-4736c570a716?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      price: 170,
+      owner:"68123b126e02a3ad3e0a73e6",
+    },
+    {
+      item: "Veg Biryani",
+      description: "Mixed vegetables cooked with basmati rice and subtle spices.",
+      image: "https://plus.unsplash.com/premium_photo-1664391895725-ed1819010135?q=80&w=1933&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      price: 160,
+      owner:"68123b126e02a3ad3e0a73e6",
+    },
+    {
+      item: "Chettinad Biryani",
+      description: "Spicy and fiery biryani from Tamil Nadu, with robust Chettinad flavors.",
+      image: "https://images.unsplash.com/photo-1630409351217-bc4fa6422075?q=80&w=2138&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      price: 230,
+      owner:"68123b126e02a3ad3e0a73e6",
+    },
+    {
+      item: "Afghani Biryani",
+      description: "Mild and creamy biryani with a Middle-Eastern touch and tender meat.",
+      image: "https://plus.unsplash.com/premium_photo-1668143365432-4f359c3a0537?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      price: 230,
+      owner:"68123b126e02a3ad3e0a73e6",
+    }
+];
+
+module.exports = {data : biryaniMenu };
